c811035970d9907e189d509fc84bbbb697a3d8a9
Ayush Jain <ayushjainid@gmail.com>
2022-08-08 20:36:12 -0500

    Time: 689 ms (19.49%), Space: 10.2 MB (74.39%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,8 +3,10 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         vector<int> resultIndices;
+        
+        //Brute force
         for(int start = 0; start < nums.size(); start++) {
-            for(int end = start+1; end <nums.size(); end++) {
+            for(int end = start + 1; end < nums.size(); end++) {
                 if(nums[start] + nums[end] == target) {
                     resultIndices.push_back(start);
                     resultIndices.push_back(end);
@@ -12,5 +14,6 @@ public:
             }
         }
         return resultIndices;
+        
     }
 };
