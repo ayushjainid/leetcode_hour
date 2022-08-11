a6f84b6ff19a7a2cb340cfe0f905063df4526951
Ayush Jain <ayushjainid@gmail.com>
2022-08-10 20:40:50 -0500

    Time: 8 ms (96.65%), Space: 10.9 MB (33.54%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,31 +3,31 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
 //         //1. Brute force
-        vector<int> resultIndices;
+//         vector<int> resultIndices;
         
-        for(int start = 0; start < nums.size(); start++) {
-            for(int end = start + 1; end < nums.size(); end++) {
-                if(nums[start] + nums[end] == target) {
-                    resultIndices.push_back(start);
-                    resultIndices.push_back(end);
-                }
-            }
-        }
-        return resultIndices;
-        
-            //2. Hash map based
-//             unordered_map<int, int> hash;
-        
-//             for(int i = 0; i < nums.size(); i++) {
-//             int numToFind = target - nums[i];
-            
-//             if(hash.find(numToFind) != hash.end()) {
-//                 return {hash[numToFind], i};
-//             }
-//             else {
-//                 hash[nums[i]] = i;
+//         for(int start = 0; start < nums.size(); start++) {
+//             for(int end = start + 1; end < nums.size(); end++) {
+//                 if(nums[start] + nums[end] == target) {
+//                     resultIndices.push_back(start);
+//                     resultIndices.push_back(end);
+//                 }
 //             }
 //         }
-//         return {};
+//         return resultIndices;
+        
+            //2. Hash map based
+            unordered_map<int, int> hash;
+        
+            for(int i = 0; i < nums.size(); i++) {
+            int numToFind = target - nums[i];
+            
+            if(hash.find(numToFind) != hash.end()) {
+                return {hash[numToFind], i};
+            }
+            else {
+                hash[nums[i]] = i;
+            }
+        }
+        return {};
     }
 };
