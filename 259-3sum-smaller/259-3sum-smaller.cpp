4c68becd03bc450ff59b9e1e24cc36ce6bf9e8c5
Ayush Jain <ayushjainid@gmail.com>
2022-08-16 20:57:38 -0500

    Time: 49 ms (89.17%), Space: 13.7 MB (14.17%) - LeetHub

---
+++ 259-3sum-smaller/259-3sum-smaller.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        if(nums.size()<3)
+        return 0;
+        int count = 0;
+        for(int first = 0; first < nums.size() - 2; first++) {
+            count += findPair(nums, target-nums[first], first);
+        }
+        return count;
+    }
+    
+private:
+    int findPair(vector<int> &nums, int targetSum, int first) {
+        int count = 0;
+        int left = first+1, right = nums.size()-1;
+        while(left < right) {
+            if(nums[left] + nums[right] < targetSum) {
+                count += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+};
